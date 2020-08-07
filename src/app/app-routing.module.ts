df1a063b7f5e7546c882d49f616d170603f26135
Zygen <Xsaber64@Gmail.com>
2020-08-07 18:38:35 -0500

    Added home page hero section and background image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   { path: 'homePage', component: HomePageComponent },
-  { path: '', redirectTo: '/homePage', pathMatch: 'full' },
+  // { path: '', redirectTo: '/homePage', pathMatch: 'full' },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
