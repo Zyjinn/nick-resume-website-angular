e6f57a33618f47185063c0347baabf5ac5293d0c
Zygen <Xsaber64@Gmail.com>
2020-08-07 17:57:15 -0500

    Created footer and header as components and implemented in app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, HeaderComponent],
+  declarations: [AppComponent, HomePageComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
