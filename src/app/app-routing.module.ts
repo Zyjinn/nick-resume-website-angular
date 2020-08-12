64e36e10dde30174200cb3d2a18ffb0742fc6dc1
Zygen <Xsaber64@Gmail.com>
2020-08-12 15:44:40 -0500

    Added simple hire-me routing to header and then made background image full page w/ bg image again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { RouterModule, Routes, Router } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { HireMeComponent } from './hire-me/hire-me.component';
 
 const routes: Routes = [
   { path: 'homePage', component: HomePageComponent },
   { path: '', redirectTo: '/homePage', pathMatch: 'full' },
   { path: 'contactMe', component: ContactMeComponent },
   { path: 'Projects', component: ProjectsComponent },
+  { path: 'hireMe', component: HireMeComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
