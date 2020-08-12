2fccf57cc8a78a884e51853d3e199eae0f4e4cae
Zygen <Xsaber64@Gmail.com>
2020-08-11 22:04:37 -0500

    Added contact form finishing touches and form validation, However, email doesn't validate properly

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-me',
   templateUrl: './contact-me.component.html',
-  styleUrls: ['./contact-me.component.css']
+  styleUrls: ['./contact-me.component.css'],
 })
 export class ContactMeComponent implements OnInit {
+  // Properties
+  name: string;
+  email: string;
+  message: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitForm(): void {
+    const formAlert = `Thank you ${this.name} for your submission! I should get back to you within 24 hours!`;
+    alert(formAlert);
   }
-
 }
