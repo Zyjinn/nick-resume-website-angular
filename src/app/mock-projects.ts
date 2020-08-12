6796588e6bc27dfc2aa8037a9d87a38ac8018cce
Zygen <Xsaber64@Gmail.com>
2020-08-12 17:31:51 -0500

    Added buttons to link to each github repo

--- src/app/mock-projects.ts
+++ src/app/mock-projects.ts
@@ -1,11 +1,19 @@
 import { Project } from './project';
 
 export const PROJECTS: Project[] = [
-  { name: 'This Website', language: 'Javascript and Angular', url: '#' },
-  { name: 'Exotic Animal Finder', language: 'Javascript and React', url: '#' },
+  {
+    name: 'This Website',
+    language: 'Javascript and Angular',
+    url: 'https://github.com/Zyjinn/nick-resume-website-angular',
+  },
+  {
+    name: 'Exotic Animal Finder',
+    language: 'Javascript and React',
+    url: 'https://gitlab.com/fox-mains-anonymous/exotic-animal-finder',
+  },
   {
     name: 'University of Fakeland',
     language: 'HTML, CSS, PHP and mySQlite',
-    url: '#',
+    url: 'https://github.com/Zyjinn/CMPS-460-DBProject',
   },
 ];
