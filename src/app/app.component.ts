7abfcf1b00bbb44adf794221b19b568fc539ea2e
Zygen <Xsaber64@Gmail.com>
2020-08-23 15:07:33 -0500

    Seemingly Pointless commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CoreModule } from './core/core.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'nick-resume-website-angular';
