3d03bac5a1a2c320c17c8150e1148da0d5198d4c
Zygen <Xsaber64@Gmail.com>
2020-10-28 22:15:37 -0500

    Created TS file form logic in contact-me

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,18 +1,64 @@
+// Imports
 import { Component, OnInit } from '@angular/core';
 import { ConnectionService } from '../connection.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HostListener } from '@angular/core';
 
+// Component file definitions
 @Component({
   selector: 'app-contact-me',
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.css'],
 })
+
+// TS class definition
 export class ContactMeComponent implements OnInit {
   // Properties
-  name: string;
-  email: string;
-  message: string;
+  contactForm: FormGroup;
+  disabledSubmitButton = true;
+  optionsSelect: Array<any>;
 
-  constructor() {}
+  // Listener
+  @HostListener('input') oninput() {
+    if (this.contactForm.valid) {
+      this.disabledSubmitButton = false;
+    }
+  }
+
+  // Constructor for form
+  constructor(
+    private fb: FormBuilder,
+    private connectionService: ConnectionService
+  ) {
+    this.contactForm = fb.group({
+      contactFormName: ['', Validators.required],
+      contactFormEmail: [
+        '',
+        Validators.compose([Validators.required, Validators.email]),
+      ],
+      contactFormSubjects: ['', Validators.required],
+      contactFormMessage: ['', Validators.required],
+      contactFormCopy: [''],
+    });
+  }
+
+  // OnSubmit()
+  // On submission for form function
+  onSubmit() {
+    const formAlert =
+      'Thank you ${this.name} for your submission! I should get back to you within 24 hours!';
+    // Send the message
+    this.connectionService.sendMessage(this.contactForm.value).subscribe(
+      () => {
+        alert(formAlert);
+        this.contactForm.reset();
+        this.disabledSubmitButton = true;
+      },
+      (error) => {
+        console.log('Error in contact form', error);
+      }
+    );
+  }
 
   ngOnInit(): void {}
 
