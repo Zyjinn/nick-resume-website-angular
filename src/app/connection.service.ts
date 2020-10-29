69c9f4a35a54163f043048bdbd0b433dc27f1cd5
Zygen <Xsaber64@Gmail.com>
2020-10-28 22:20:39 -0500

    Created the connection.services file and the logic for sending a message

--- src/app/connection.service.ts
+++ src/app/connection.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConnectionService {
+  url = 'http://localhost:3000/send';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  sendMessage(messageContent: string) {
+    return this.http.post(this.url, JSON.stringify(messageContent), {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      responseType: 'text',
+    });
+  }
 }
