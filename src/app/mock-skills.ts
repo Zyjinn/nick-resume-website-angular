3f21ef269f56c01067fe84e3c495f98942b77260
Zygen <Xsaber64@Gmail.com>
2020-11-11 23:37:19 -0600

    About me section mostly done and buttons for hire me section basically done?

--- src/app/mock-skills.ts
+++ src/app/mock-skills.ts
@@ -1,8 +1,18 @@
 import { Skill } from './skill';
 
 export const SKILLS: Skill[] = [
-  { skill: 'Java', experience: '4 years' },
-  { skill: 'CSS', experience: '1 year' },
-  { skill: 'HTML', experience: '2 years' },
+  { skill: 'Java', experience: '5 Years' },
+  { skill: 'Python', experience: '2 Years' },
+  { skill: 'HTML', experience: '2 Years' },
+  { skill: 'Git(Github, Bitbucket, Gitkraken, CLI', experience: '2 Years' },
+  { skill: 'CSS', experience: '1 Year' },
   { skill: 'Javascript', experience: '1 Year' },
+  { skill: 'C++', experience: '1 Year' },
+  { skill: 'Angular', experience: '1 Year' },
+  { skill: 'Bootstrap', experience: '1 Year' },
+  { skill: 'Bulma', experience: '1 Year' },
+  { skill: 'React', experience: '1 Year' },
+  { skill: 'SQL(SQLite, MySQL)', experience: '1 Year' },
+  { skill: 'SQLite', experience: '1 Year' },
+  { skill: 'MySQL', experience: '1 Year' },
 ];
